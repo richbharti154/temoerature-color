f0bde825ed215bc703b7fc96290c7b483cf1a175
richbharti154 <116936700+richbharti154@users.noreply.github.com>
2023-05-26 13:54:23 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React,{useState}from "react";
 import "./style.css";
-
 export default function App() {
+  const arr=['red','green','yellow','black','brown']
   const[count,setCount]=useState(0)
+  const[color,setColor]=useState('white')
+  function handleClick2(element){
+    setColor(element)
+  }
   let colour
   if(count>=1 &&count>=0){
     colour='red'
@@ -12,16 +16,25 @@ export default function App() {
   }
   function handleclick(){
     setCount(count+1)
+    console.log(count+1)
   }
   function handleclick1(){
     setCount(count-1)
   }
   return (
+    <>
     <div style={{backgroundColor:colour}}>
       <h1 className='heading'>Increment and Decrement</h1>
       <p className='count'>{count}</p>
+      <div className='container'>
       <button className='incre' onClick={handleclick}>add</button>
       <button className='decre' onClick={handleclick1}>decre</button>
     </div>
+    <div  style={{backgroundColor:color,margin:'20px'}}>
+      {arr.map((element)=><p className='change'><button onClick={()=>handleClick2(element)}>{element}
+      </button></p>)}
+      </div>
+      </div>
+      </>
   );
 }
